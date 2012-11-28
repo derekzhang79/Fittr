da0e986ea40c27c49ecafef24b91711fbb0b4c07
nolanperk <nolanperk@gmail.com>
2012-11-28 13:33:14 -0500

    footr

---
+++ app/assets/javascripts/su.js
@@ -0,0 +1,14 @@
+(function() {
+  var li = document.createElement('script'); li.type = 'text/javascript'; li.async = true;
+  li.src = ('https:' == document.location.protocol ? 'https:' : 'http:') + '//platform.stumbleupon.com/1/widgets.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(li, s);
+})();
+
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=257797927676830";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
