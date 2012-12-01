0cc7c0c966f161e35feb6bd5c167a8bb34ced8dc
So Townsend <so.townsend@gmail.com>
2012-12-01 11:15:36 -0500

    about link fix

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,22 +1,2 @@
 class AboutController < ApplicationController
-  def howFittrWorks
-  end
-
-  def fittrsWorkouts
-  end
-  
-  def termsOfUse
-  end
-
-  def fittrTechnology
-  end
-  
-  def socialize
-  end
-  
-  def ContactUs
-  end
-  
-  def FAQ
-  end
 end
